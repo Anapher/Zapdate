1a87886fc18dd034fa8a3aa2f71a0e0f87c0ddbb
Vincent (Anapher) <anapher@protonmail.com>
2019-06-30 15:47:50 +0200

    Begin working on update packages...

---
+++ src/Zapdate/Controllers/UpdatePackagesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Zapdate.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/v1/projects/{projectId}/[controller]")]
+    public class UpdatePackagesController
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult> CreateUpdatePackage()
+        {
+
+        }
+    }
+}
---
+++ src/Zapdate/Models/Request/CreateUpdatePackageRequestDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zapdate.Models.Request
+{
+    public class CreateUpdatePackageRequest
+    {
+        public string Version { get; set; }
+        public string? Description { get; set; }
+        public IDictionary<string, string>? CustomFields { get; set; }
+
+        public IList<UpdateFileDto> Files { get; set; }
+        public IList<UpdateChangelogDto> Changelogs { get; set; }
+    }
+
+    public class UpdateChannelDto
+    {
+        public string Name { get; set; }
+        public DateTimeOffset? PublishDate { get; set; }
+    }
+
+    public class UpdateChangelogDto
+    {
+        public string Language { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class UpdateFileDto
+    {
+        public string Path { get; set; }
+        public string Sha256Checksum { get; set; }
+        public string? Signature { get; set; }
+    }
+}
